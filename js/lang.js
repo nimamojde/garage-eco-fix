53536b9ac6873acfe0a5141622a92f93d1652221
Mozhdeh <158485582+nimamojde@users.noreply.github.com>
2026-01-11 15:44:36 -0500

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -1,1 +1,22 @@
+function setLanguage(lang) {
+  console.log("Language:", lang);
+  localStorage.setItem("lang", lang);
+
+  fetch("lang/" + lang + ".json")
+    .then(response => response.json())
+    .then(data => {
+      document.querySelectorAll(".lang").forEach(el => {
+        const key = el.getAttribute("data-key");
+        if (data[key]) {
+          el.innerHTML = data[key];
+        }
+      });
+    })
+    .catch(err => console.error("LANG ERROR", err));
+}
+
+window.onload = function () {
+  const savedLang = localStorage.getItem("lang") || "en";
+  setLanguage(savedLang);
+};
 
